3b7bdb097ed9301b68251a4a664642425feff5a9
inventrovictor <inventrovictor@gmail.com>
2021-06-19 15:04:08 -0700

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -  prints disimmilar digits between 00 and 99
+ * Return: 0
+ */
+int main(void)
+{
+  int i;
+  int j;
+
+for (i = 0; i < 10; ++i)
+{
+for (j = i + 1; j < 10; ++j)
+{
+putchar('0' + i);
+putchar('0' + j);
+
+if ( i != 8 || j!= 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+  
+return (0);
+}   
