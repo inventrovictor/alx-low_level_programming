45327883bf2502ca9b7ebb67d86ae00aae335846
inventrovictor <inventrovictor@gmail.com>
2021-06-19 13:55:59 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints out the alphabet in lower case except for q and e
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+if(c != 'e' && c != 'q')
+putchar(c);
+putchar('\n');
+return (0);
+}
+     
