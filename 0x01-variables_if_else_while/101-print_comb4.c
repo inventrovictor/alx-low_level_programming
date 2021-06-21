ee4b7d4cf522f460111eb7607f98783fc166c506
inventrovictor <inventrovictor@gmail.com>
2021-06-21 00:34:13 -0700

    new

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,32 +6,34 @@
  */
 int main(void)
 {
-int n1 = 48;
-int n2, n3;
-int com = 44;
+int i, j, k;
 
-while (n1 <= 57)
+for (i = 48; i < 58; i++)
 {
-n2 = n1 + 1;
-while (n2 <= 57)
+for (j = i; j < 58; j++)
 {
-n3 = n2 + 1;
-while (n3 <= 57)
+for (k = j; k < 58; k++)
 {
-putchar(n1);
-putchar(n2);
-putchar(n3);
-if (n1 != 55 || n2 != 56 || n3 != 57)
+if (i == j || j == k || i == k)
 {
-putchar(com);
-putchar(32);
+continue;
 }
-n3 =+ 1;
+putchar(i);
+putchar(j);
+putchar(k);
+
+if (i == 55 && j == 56 && k == 57)
+{
+break;
 }
-n2 =+ 1;
+else
+{
+putchar(',');
+putchar(',');
 }
-n1 =+ 1;
-} 
+}
+}
+}
 putchar('\n');
 return (0);
 }
