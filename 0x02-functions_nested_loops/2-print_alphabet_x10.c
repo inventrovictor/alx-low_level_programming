c18a11faca3838ad081032dc9d1605912a29910d
inventrovictor <inventrovictor@gmail.com>
2021-06-22 05:14:13 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times from a-z
+ * Return: the lower case alphabet
+ */
+void print_alphabet_x10(void)
+{
+char c;
+int i;
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
