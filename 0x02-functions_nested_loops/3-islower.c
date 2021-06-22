ba4c30b3f68768885dcdcc22a4e23001a82da876
inventrovictor <inventrovictor@gmail.com>
2021-06-22 05:22:50 -0700

    new

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _islower - checks for lower case letter
+ * c - lower case letters
+ * Return: 1 if c, 0 if otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+return (0);
+}
